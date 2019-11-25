6257ce1ed0a7d1bf5c836e407fc2872d8c310de1
JimenaLomeli <jimena@lomeli.mx>
2019-11-25 17:22:39 -0600

    Add files via upload

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // revisa tu connectionURL aqui :-)
 if ( process.env.NODE_ENV === 'production') {
-  var connectionURL = process.env.API_KEY
+  var connectionURL = process.env.connectionURL
 } else {
   const credentials = require('../config.js')
   var connectionURL = credentials.connectionURL
